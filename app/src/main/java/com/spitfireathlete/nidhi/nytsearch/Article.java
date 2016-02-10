732667705c94086100c50b57db71098556b1059f
nidhik <nidhikulkarni82@gmail.com>
2016-02-09 18:09:06 -0800

    Use NYT search API and parse results into a list of articles.

---
+++ app/src/main/java/com/spitfireathlete/nidhi/nytsearch/Article.java
@@ -0,0 +1,70 @@
+package com.spitfireathlete.nidhi.nytsearch;
+
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by nidhikulkarni on 2/9/16.
+ */
+public class Article {
+
+    private String webURL;
+    private String headline;
+    private String thumbnail;
+
+    public Article(JSONObject json) {
+        try {
+            this.webURL = json.getString("web_url");
+            this.headline = json.getJSONObject("headline").getString("main");
+            JSONArray mm = json.getJSONArray("multimedia");
+            if (mm.length() > 0) {
+                JSONObject mmJson = mm.getJSONObject(0);
+                this.thumbnail = "http://www.nytimes.com/" + mmJson.getString("url");
+            } else {
+                this.thumbnail = "";
+            }
+
+        } catch (JSONException e) {
+            //FIXME: do something
+            Log.e("ERROR", e.toString());
+        }
+    }
+
+    public static List<Article> fromJSONArray(JSONArray array) {
+        List<Article> articles = new ArrayList<Article>();
+
+        for (int i = 0; i < array.length(); i++) {
+            try {
+                articles.add(new Article(array.getJSONObject(i)));
+            } catch (JSONException e) {
+                //FIXME: do something
+                Log.e("ERROR", e.toString());
+            }
+        }
+
+        return articles;
+    }
+
+    public String getWebURL() {
+        return webURL;
+    }
+
+
+    public String getHeadline() {
+        return headline;
+    }
+
+
+
+    public String getThumbnail() {
+        return thumbnail;
+    }
+
+
+}
--- app/src/main/java/com/spitfireathlete/nidhi/nytsearch/SearchActivity.java
+++ app/src/main/java/com/spitfireathlete/nidhi/nytsearch/SearchActivity.java
@@ -3,19 +3,36 @@ package com.spitfireathlete.nidhi.nytsearch;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.GridView;
-import android.widget.Toast;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import cz.msebera.android.httpclient.Header;
 
 public class SearchActivity extends AppCompatActivity {
 
     private EditText etQuery;
     private GridView gvResults;
     private Button btnSearch;
+    private List<Article> articles;
+
+    private static final String NYT_API_KEY = "14886966169991e54b1a4710491c9efc:4:74352748";
+    private static final String NYT_SEARCH_URL = "http://api.nytimes.com/svc/search/v2/articlesearch.json";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +44,7 @@ public class SearchActivity extends AppCompatActivity {
         etQuery = (EditText) findViewById(R.id.etQuery);
         gvResults = (GridView) findViewById(R.id.gvResults);
         btnSearch = (Button) findViewById(R.id.btnSearch);
+        this.articles = new ArrayList<Article>();
 
     }
 
@@ -55,7 +73,37 @@ public class SearchActivity extends AppCompatActivity {
     public void handleSearchClicked(View view) {
 
         String query = etQuery.getText().toString();
-        Toast.makeText(this, "Search for " + query, Toast.LENGTH_LONG).show();
+        searchFor(query);
 
+    }
+
+    private void searchFor(String query) {
+        AsyncHttpClient httpClient = new AsyncHttpClient();
+        RequestParams params = new RequestParams();
+        params.add("api-key", NYT_API_KEY);
+        params.add("page", "0");
+        params.add("q", query);
+        httpClient.get(NYT_SEARCH_URL, params, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+
+                JSONArray articlesJSON = null;
+                try {
+                    articlesJSON = response.getJSONObject("response").getJSONArray("docs");
+                    articles.addAll(Article.fromJSONArray(articlesJSON));
+
+                    Log.d("DEBUG", articles.toString());
+
+                } catch (JSONException e) {
+                    Log.e("ERROR", e.toString());
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                // FIXME: do something
+                Log.e("ERROR", errorResponse.toString());
+            }
+        });
     }
 }
